a51d48ecb4bed201fbc4f2c4c3b48c2732ab48c1
Shoban Chiddarth <messageshoban@gmail.com>
2025-03-09 12:58:36 +0530

    Added bgm and sfx audio
    bgm plays in menus, and sfx plays when ball hits something
    (different audio for ball hitting stick and ball hitting wall)

--- scripts/Menus/GameOver.gd
+++ scripts/Menus/GameOver.gd
@@ -1,6 +1,8 @@
 extends Control
 
+@onready var bgm: AudioStreamPlayer = $bgm
 
+var playing := false
 
 func _on_replay_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/Worlds/Arena.tscn")
@@ -8,3 +10,10 @@ func _on_replay_button_pressed() -> void:
 
 func _on_main_menu_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/Menus/MainMenu.tscn")
+
+func _process(_delta):
+	if visible and (not playing):
+		bgm.play()
+		playing = true
+	elif (not visible) and (playing):
+		bgm.stop()
